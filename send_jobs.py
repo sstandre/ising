182d5e18b2030349436bc92299929a018f0e7ac8
sstandre <simon.saint.a@gmail.com>
2021-10-24 11:56:10 -0300

    nuevas simulaciones con B!=0

--- send_jobs.py
+++ send_jobs.py
@@ -5,10 +5,10 @@ import os
 from subprocess import call
 from shutil import copy
 
-B = 0.0                 # Campo magnético
-L = 20                  # Tamaño de la red (lado)
-steps = 1_000_000         # Pasos de MC para producción (~L*L * 3k)
-steps_term = 0.1*steps      # Pasos de MC para termalización (~steps)
+B = 1.0                           # Campo magnético
+L = 100                           # Tamaño de la red (lado)
+steps = 30_000_000                # Pasos de MC para producción (~L*L * 3k)
+steps_term = int(0.1*steps)       # Pasos de MC para termalización (~0.1*steps)
 # La secuencia de temperatura se define con una T inicial, seguida de pares (step, end)
 # que definen rampas del estilo: [start, start+step, start+2*step+..., end]
 # Luego la secuencia se invierte y las simulaciones se corren de T max a T min.
@@ -21,12 +21,12 @@ temp_sequence = (
     (0.1, 4.1),         #   (step, end)
 )
 data_files = ['input.dat', 'output.dat', 'averages.dat', 'matriz.dat']
-
+SKIP_EXISTING = True
 
 def make_interval(start, *ramps):
     interval = []
     for step, end in ramps:
-        n_steps = int( (end-start)/step )
+        n_steps = round( (end-start)/step )
         interval += [start + step*i for i in range(n_steps)]
         start = end
     return interval
@@ -62,9 +62,9 @@ def main(args):
 
         for temp in temperatures:
             tempdir = f'data/{L}_size/{B:.3}_B/{temp:.3}_temp'
-            # if os.path.exists(tempdir):
-            #     print(f'El directorio {tempdir} ya existe, continuando con el siguiente.')
-            #     continue
+            if os.path.exists(tempdir) and SKIP_EXISTING:
+                print(f'El directorio {tempdir} ya existe, continuando con el siguiente.')
+                continue
             print('*'*30)
             print(f'Corrida a T={temp:.3}\n')
             
